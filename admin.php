44f7ec96dfaeea1f19ed487a513200be01606dbd
petercoughlin <petercoughlin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-09-17 09:21:34 +0000

    updated help text on admin page
    git-svn-id: https://plugins.svn.wordpress.org/pc-searchengine-verify/trunk@155653 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin.php
+++ admin.php
@@ -24,8 +24,8 @@ class pc_searchengine_verify_admin {
 			check_admin_referer('pc_searchengine_verify_settings');
 
 			$options['google'] = trim($_POST['google']);
-			$options['bing'] = trim($_POST['bing']);
 			$options['yahoo'] = trim($_POST['yahoo']);
+			$options['bing'] = trim($_POST['bing']);
 
 			isset($_POST['remove_settings']) ? $options['remove_settings'] = true : $options['remove_settings'] = false;
 
@@ -39,18 +39,25 @@ class pc_searchengine_verify_admin {
 			.'<h2>PC Search Engine Verify Settings</h2>'
 			.'<form method="post">';
 		if ( function_exists('wp_nonce_field') ) wp_nonce_field('pc_searchengine_verify_settings');
-		echo '<table class="form-table">'
+		echo '<p>To verify ownership of your website, the search engines will ask you to add specific information to your site. One option will be to add a meta tag to your home page.</p><p>Log in to the following webmaster tools areas to get your meta tag code;</p>'
+			.'<table>'
+			.'<tr><td><strong>Google:</strong></td><td><a href="https://www.google.com/webmasters/tools/" target="_blank">https://www.google.com/webmasters/tools/</a></td></tr>'
+			.'<tr><td><strong>Yahoo:</strong></td><td><a href="https://siteexplorer.search.yahoo.com/" target="_blank">https://siteexplorer.search.yahoo.com/</a></td></tr>'
+			.'<tr><td><strong>Bing:</strong></td><td><a href="http://www.bing.com/webmaster/" target="_blank">http://www.bing.com/webmaster/</a></td></tr>'
+			.'</table>'
+			.'<p>When you have added your site in the webmaster tools area of these search engines you will receive the meta tag code needed to verify ownership. Add the content value of the meta tag code into the appropriate box below and click the Save Changes button. You can then verify your site in the webmaster tools area.</p>'
+			.'<table class="form-table">'
 			.'<tr>'
 				.'<th scope="row">Google:</th>'
 				.'<td>&lt;meta name="verify-v1" content="<input type="text" name="google" id="google" value="' . stripslashes($options['google']) . '" class="regular-text" />" /&gt;</td>'
 			.'</tr>'
 			.'<tr>'
-				.'<th scope="row">Bing:</th>'
-				.'<td>&lt;meta name="msvalidate.01" content="<input type="text" name="bing" id="bing" value="' . stripslashes($options['bing']) . '" class="regular-text" />" /&gt;</td>'
-			.'</tr>'
-			.'<tr>'
 				.'<th scope="row">Yahoo:</th>'
 				.'<td>&lt;meta name="y_key" content="<input type="text" name="yahoo" id="yahoo" value="' . stripslashes($options['yahoo']) . '" class="regular-text" style="width:150px;" />"  /&gt;</td>'
+			.'</tr>'
+			.'<tr>'
+				.'<th scope="row">Bing:</th>'
+				.'<td>&lt;meta name="msvalidate.01" content="<input type="text" name="bing" id="bing" value="' . stripslashes($options['bing']) . '" class="regular-text" />" /&gt;</td>'
 			.'</tr>'
 			.'<tr>'
 				.'<th scope="row">Delete settings on deactivation:</th>'
